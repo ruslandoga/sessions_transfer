0ea4e0bc384bdab0bbe08512f60336519c9d65ac
ruslandoga <ruslandoga+gh@icloud.com>
2025-03-05 13:47:16 +0300

    mix new

---
+++ lib/sessions_transfer.ex
@@ -0,0 +1,18 @@
+defmodule SessionsTransfer do
+  @moduledoc """
+  Documentation for `SessionsTransfer`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SessionsTransfer.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sessions_transfer_test.exs
@@ -0,0 +1,8 @@
+defmodule SessionsTransferTest do
+  use ExUnit.Case
+  doctest SessionsTransfer
+
+  test "greets the world" do
+    assert SessionsTransfer.hello() == :world
+  end
+end
