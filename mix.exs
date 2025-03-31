73d9f3eecea12ed14e3d6b30c06acb39030caeb8
ruslandoga <ruslandoga+gh@icloud.com>
2025-03-31 21:57:14 +0300

    test (#1)
    * test
    
    * test!
    
    * works!

--- mix.exs
+++ mix.exs
@@ -14,6 +14,7 @@ defmodule SessionsTransfer.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      mod: {Plausible.Application, []},
       extra_applications: [:logger]
     ]
   end
@@ -22,7 +23,11 @@ defmodule SessionsTransfer.MixProject do
   defp deps do
     [
       {:benchee, github: "bencheeorg/benchee"},
-      {:telemetry, "~> 1.3"}
+      {:ecto, "~> 3.12"},
+      {:ch, "~> 0.3.2"},
+      {:telemetry, "~> 1.3"},
+      {:con_cache,
+       git: "https://github.com/aerosol/con_cache", branch: "ensure-dirty-ops-emit-telemetry"}
     ]
   end
 end
--- test/tinysock_test.exs
+++ test/tinysock_test.exs
@@ -1,12 +1,12 @@
 defmodule TinySockTest do
   use ExUnit.Case, async: true
+  alias Plausible.Session.Persistence.TinySock
+
   @moduletag :tmp_dir
 
   test "it works", %{tmp_dir: tmp_dir} do
-    server =
-      start_supervised!({TinySock, base_path: tmp_dir, handler: fn :ping -> {:reply, :pong} end})
-
-    assert {:ok, {:local, sock_path}} = :inet.sockname(TinySock.socket(server))
+    server = start_supervised!({TinySock, base_path: tmp_dir, handler: fn :ping -> :pong end})
+    assert {:ok, {:local, sock_path}} = :inet.sockname(TinySock.listen_socket(server))
     assert {:ok, [^sock_path]} = TinySock.list(tmp_dir)
     assert {:ok, :pong} = TinySock.call(sock_path, :ping)
   end
