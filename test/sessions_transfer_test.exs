390ce593b0aa7055fd7a8bd7c38dcb8dd5b7c64b
ruslandoga <ruslandoga+gh@icloud.com>
2025-03-31 19:33:41 +0300

    test!

--- test/sessions_transfer_test.exs
+++ test/sessions_transfer_test.exs
@@ -1,17 +1,65 @@
 defmodule SessionsTransferTest do
   use ExUnit.Case
 
-  # @tag :tmp_dir
-  # test "try_lock/1", %{tmp_dir: tmp_dir} do
-  #   path = Path.join([tmp_dir, "socket"])
+  setup do
+    Application.put_env(:plausible, Plausible.Cache.Adapter, sessions: [partitions: 100])
+  end
 
-  #   # please see https://linux.die.net/man/7/unix for UNIX_PATH_MAX and stuff
-  #   # https://serverfault.com/questions/641347/check-if-a-path-exceeds-maximum-for-unix-domain-socket
-  #   assert byte_size(path) < 104
-  #   assert {:ok, listen_socket} = SessionsTransfer.try_lock(path)
-  #   assert {:error, :eaddrinuse} = SessionsTransfer.try_lock(path)
+  test "it works" do
+    tmp_dir = Path.join(System.tmp_dir!(), "tinysock_test")
+    File.mkdir_p!(tmp_dir)
+    on_exit(fn -> File.rm_rf!(tmp_dir) end)
 
-  #   assert :ok = SessionsTransfer.unlock(listen_socket)
-  #   assert {:ok, _another_listen_socket} = SessionsTransfer.try_lock(path)
-  # end
+    old = start_another_plausible(tmp_dir)
+    session = put_session(old, %{user_id: "123"})
+  end
+
+  defp start_another_plausible(data_dir) do
+    {:ok, pid, _node} = :peer.start_link(%{connection: {{127, 0, 0, 1}, 0}})
+    add_code_paths(pid)
+    transfer_configuration(pid)
+    ensure_applications_started(pid)
+    pid
+  end
+
+  defp add_code_paths(pid) do
+    :ok = :peer.call(pid, :code, :add_paths, [:code.get_path()])
+  end
+
+  defp transfer_configuration(pid) do
+    for {app_name, _, _} <- Application.loaded_applications() do
+      for {key, val} <- Application.get_all_env(app_name) do
+        :ok = :peer.call(pid, Application, :put_env, [app_name, key, val])
+      end
+    end
+  end
+
+  defp ensure_applications_started(pid) do
+    {:ok, _apps} = :peer.call(pid, Application, :ensure_all_started, [:mix])
+    :ok = :peer.call(pid, Mix, :env, [Mix.env()])
+
+    for {app_name, _, _} <- Application.loaded_applications() do
+      {:ok, _apps} = :peer.call(pid, Application, :ensure_all_started, [app_name])
+    end
+  end
+
+  defp put_session(pid, overrides) do
+    default = %Plausible.ClickhouseSessionV2{
+      sign: 1,
+      session_id: Ecto.UUID.generate(),
+      user_id: Ecto.UUID.generate(),
+      hostname: "example.com",
+      site_id: Enum.random(1000..10_000),
+      entry_page: "/",
+      pageviews: 1,
+      events: 1,
+      start: DateTime.utc_now(:second),
+      timestamp: DateTime.utc_now(:second),
+      is_bounce: false
+    }
+
+    session = struct!(default, overrides)
+    key = {session.site_id, session.user_id}
+    :peer.call(pid, Plausible.Cache.Adapter, :put, [:sessions, key, session, [dirty?: true]])
+  end
 end
